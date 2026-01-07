da51b7fd7f41e6b45c17e2a0c18795220e72cf5c
yutsakam <yuta.x.sakamoto@oracle.com>
2026-01-07 13:03:28 +0900

    Some update.

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -1,5 +1,5 @@
 -- Pull in the wezterm API
-local wezterm = require 'wezterm'
+local wezterm = require("wezterm")
 
 -- This will hold the configuration.
 local config = wezterm.config_builder()
@@ -7,61 +7,66 @@ local config = wezterm.config_builder()
 -- This is where you actually apply your config choices.
 
 return {
-    -- Basic settings
-    automatically_reload_config = true,
-    use_ime = true,
-    default_cursor_style = 'BlinkingBlock',
-    leader = {
-        key = "a",
-        mods = "CTRL",
-        timeout_milliseconds = 2000
-    },
+	-- Basic settings
+	automatically_reload_config = true,
+	use_ime = true,
+	default_cursor_style = "BlinkingBlock",
+	leader = {
+		key = "a",
+		mods = "CTRL",
+		timeout_milliseconds = 2000,
+	},
 
-    -- Window settings
-    initial_cols = 120,
-    initial_rows = 40,
-    cell_width = 1.1,
-    line_height = 1.1,
-    adjust_window_size_when_changing_font_size = false,
+	-- Window settings
+	initial_cols = 120,
+	initial_rows = 40,
+	cell_width = 1.1,
+	line_height = 1.1,
+	adjust_window_size_when_changing_font_size = false,
 
-    -- Tab settings
-    use_fancy_tab_bar = false,
-    tab_bar_at_bottom = true,
-    tab_max_width = 100,
-    hide_tab_bar_if_only_one_tab = true,
-    show_tabs_in_tab_bar = true,
+	-- Tab settings
+	use_fancy_tab_bar = false,
+	tab_bar_at_bottom = true,
+	tab_max_width = 100,
+	hide_tab_bar_if_only_one_tab = true,
+	show_tabs_in_tab_bar = true,
 
-    -- Color settings
-    -- color_scheme = "Ryuuko",
-    color_scheme = "catppuccin-mocha",
-    window_background_opacity = 0.80,
-    macos_window_background_blur = 20,
+	-- Color settings
+	-- color_scheme = "Ryuuko",
+	-- color_scheme = "Catppuccin-Macchiato",
+	color_scheme = "catppuccin-frappe",
+	-- color_scheme = "EverForest Dark (Gogh)",
+	window_background_opacity = 0.80,
+	macos_window_background_blur = 20,
 
-    -- Font settings
-    font = wezterm.font("Moralerspace Krypton", {
-        weight = "Regular",
-        stretch = "Normal",
-        style = "Normal"
-    }),
-    font_size = 10,
+	-- Font settings
+	font = wezterm.font("Moralerspace Krypton", {
+		weight = "Regular",
+		stretch = "Normal",
+		style = "Normal",
+	}),
+	font_size = 10,
 
-    -- Key settings
-    keys = {{
-        key = "-",
-        mods = "LEADER",
-        action = wezterm.action.SplitVertical({
-            domain = "CurrentPaneDomain"
-        })
-    }, {
-        key = "\\",
-        mods = "LEADER",
-        action = wezterm.action.SplitHorizontal({
-            domain = "CurrentPaneDomain"
-        })
-    }, {
-        key = "[",
-        mods = 'CTRL',
-        action = wezterm.action.PaneSelect
-    }}
+	-- Key settings
+	keys = {
+		{
+			key = "-",
+			mods = "LEADER",
+			action = wezterm.action.SplitVertical({
+				domain = "CurrentPaneDomain",
+			}),
+		},
+		{
+			key = "\\",
+			mods = "LEADER",
+			action = wezterm.action.SplitHorizontal({
+				domain = "CurrentPaneDomain",
+			}),
+		},
+		{
+			key = "[",
+			mods = "CTRL",
+			action = wezterm.action.PaneSelect,
+		},
+	},
 }
-
