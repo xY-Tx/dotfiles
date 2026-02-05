4e771e16676535848868c8e6aaeacacb504d0672
yutsakam <yuta.x.sakamoto@oracle.com>
2026-02-05 20:25:28 +0900

    colorscheme.lua/dotfile_setup.sh

--- dotfile_setup.sh
+++ dotfile_setup.sh
@@ -10,31 +10,32 @@ REPO_DIR="$(pwd)"
 SCRIPT_NAME="$(basename "$0")"
 
 function install_sub() {
-    local execute=$1
-    local path
+  local execute=$1
+  local path
 
-    while IFS= read -rd '' path; do
-        path="${path/.\//}"
+  while IFS= read -rd '' path; do
+    path="${path/.\//}"
 
-        [[ "${path}" == "${SCRIPT_NAME}" ]] && continue
-        [[ "${path}" =~ ^\.git(\/.*)?$ ]] && continue
-        [[ "${path}" =~ \.DS_Store$ ]] && continue
-        # 他に除外したいファイルがあればここに追加します
+    [[ "${path}" == "${SCRIPT_NAME}" ]] && continue
+    [[ "${path}" =~ ^\.git(\/.*)?$ ]] && continue
+    [[ "${path}" =~ \.DS_Store$ ]] && continue
+    [[ "${path}" =~ \Brewfile$ ]] && continue
+    # 他に除外したいファイルがあればここに追加します
 
-        if [[ -d "${path}" ]]; then
-            if [[ "${execute}" = 'true' ]]; then
-                mkdir -pv "${HOME}/${path}"
-            else
-                echo "  mkdir -pv ${HOME}/${path}"
-            fi
-        elif [[ -f "${path}" ]]; then
-            if [[ "${execute}" = 'true' ]]; then
-                ln -snfv "${REPO_DIR}/${path}" "${HOME}/${path}"
-            else
-                echo "   ln -snfv ${REPO_DIR}/${path} ${HOME}/${path}"
-            fi
-        fi
-    done < <(find . -mindepth 1 -print0)
+    if [[ -d "${path}" ]]; then
+      if [[ "${execute}" = 'true' ]]; then
+        mkdir -pv "${HOME}/${path}"
+      else
+        echo "  mkdir -pv ${HOME}/${path}"
+      fi
+    elif [[ -f "${path}" ]]; then
+      if [[ "${execute}" = 'true' ]]; then
+        ln -snfv "${REPO_DIR}/${path}" "${HOME}/${path}"
+      else
+        echo "   ln -snfv ${REPO_DIR}/${path} ${HOME}/${path}"
+      fi
+    fi
+  done < <(find . -mindepth 1 -print0)
 }
 
 echo "Dry run:"
@@ -43,12 +44,12 @@ echo
 
 read -rp "ok? (y/N): " yn
 case "${yn}" in
-[yY]*)
+  [yY]*)
     install_sub 'true'
     echo
     echo "finished"
     ;;
-*)
+  *)
     echo "abort"
     ;;
 esac
