00989067107b1b2323e5322bf0a46851b7331f1a
yutsakam <yuta.x.sakamoto@oracle.com>
2026-03-23 17:24:22 +0900

    nvim lua files

--- .config/nvim/lua/config/colorscheme.lua
+++ .config/nvim/lua/config/colorscheme.lua
@@ -1,7 +1,7 @@
 -- colorscheme
-vim.cmd.colorscheme("thorn-dark-cold") --thorn-light-cold | light-warm | dark-warm | dark-cold
--- vim.cmd.colorscheme("tokyonight-day")
--- vim.cmd.colorscheme("catppuccin-frappe") -- frapp | latte | mocha | macchiato
+-- vim.cmd.colorscheme("thorn-dark-cold") --thorn-light-cold | light-warm | dark-warm | dark-cold
+-- vim.cmd.colorscheme("tokyonight")
+vim.cmd.colorscheme("catppuccin-mocha") --  frapp | latte | mocha | macchiato
 -- vim.cmd.colorscheme("tokyonight-storm") -- tokyonight-storm | tokyonight-night | tokyonight-day | tokyonight-moon
 -- vim.cmd.colorscheme("evergarden-summer") -- evergarden-winter | fall | summer | spring
 -- vim.cmd.colorscheme("nord")
